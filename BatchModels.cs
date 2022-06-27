f3a6edc1cfa67f0577108ebfbff7a5dee74fee5f
Bill Miller <bmiller@postmedia.com>
2022-06-26 23:39:22 -0400

    unused refs

--- BatchModels.cs
+++ BatchModels.cs
@@ -1,10 +1,5 @@
-﻿using Amazon.ECS.Model;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using System;
-using System.Collections.Generic;
-using System.Runtime.Serialization;
-using System.Security.Policy;
-using System.Text;
 using System.Text.Json.Serialization;
 
 namespace StreetPerfect.Models
