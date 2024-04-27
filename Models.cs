6263eb60f6e4ebf179d923dc72356f827897883a
QuickWire <bmiller@postmedia.com>
2024-04-26 21:00:55 -0400

    12.2

--- Models.cs
+++ Models.cs
@@ -12,7 +12,7 @@ namespace StreetPerfect.Models
 {
 	public class SPConst
 	{
-		public const string Version = "12.1.1";
+		public const string Version = "12.2.0";
 		public const string License = "Copyright © 1993-2024, Postmedia Network Inc";
 		public const string DataNamespace = "http://StreetPerfect.com/wcf/models/";
 		public const string ServiceNamespace = "http://StreetPerfect.com/wcf/";
