eec65146942cc5e0afa2cf72d345440b3a3bf282
Bill Miller <bmiller@postmedia.com>
2025-10-16 10:31:35 -0400

    12.3.3

--- Models.cs
+++ Models.cs
@@ -13,7 +13,7 @@ namespace StreetPerfect.Models
 {
 	public class SPConst
 	{
-		public const string Version = "12.3.2";
+		public const string Version = "12.3.3";
 		public const string License = "Copyright © 1993-2025, Postmedia Network Inc";
 		public const string DataNamespace = "http://StreetPerfect.com/wcf/models/";
 		public const string ServiceNamespace = "http://StreetPerfect.com/wcf/";
